992f169de3762ab1ea95a8d02854b94bb8fc795e
Adrien Flenghi <adrienflenghi@ikmail.com>
2025-05-25 22:58:10 +0200

    ajout de la doc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { pokemonCardRouter } from './pokemonCards/pokemonCard.router';
+import { userRouter } from './user/user.router';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+console.log(`Mon serveur démarre sur le port ${port}`);
+});
+
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 export const server = app.listen(port);
